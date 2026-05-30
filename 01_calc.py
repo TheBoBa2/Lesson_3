704866d30d6daca4c5aadc52c4762ae27db39470
TheBoBa2 <glooplootloo@gmail.com>
2026-05-30 17:20:04 +0300

    added clac

---
+++ 01_calc.py
@@ -0,0 +1,17 @@
+﻿first_number = float(input("Введіть перше число: "))
+second_number = float(input("Введіть друге число: "))
+operation = input("Введыть операцію (+, -, *, /): ")
+
+if operation == "+":
+    print(first_number + second_number)
+elif operation == "-":
+    print(first_number - second_number)
+elif operation == "*":
+    print(first_number * second_number)
+elif operation == "/":
+    if second_number == 0:
+        print("Діленя на 0 неможливе!")
+    else:
+        print(first_number / second_number)
+else:
+    print("Неіснуяча операція!")
