12ffac83f9f55bd183e311d6c59d8bf99f5bf4dd
TheBoBa2 <glooplootloo@gmail.com>
2026-06-01 17:12:52 +0300

    updated task 02

--- 02_list.py
+++ 02_list.py
@@ -1,7 +1,6 @@
 ﻿input_nums = list(map(int, input("Введіть числа: ").split()))
+
 if len(input_nums) > 1:
     last_element = input_nums.pop()
     input_nums.insert(0, last_element)
-else:
-    print("недостятня кількість елементів для змін")
-print("ітоговий список: ", input_nums)
+print(input_nums)
